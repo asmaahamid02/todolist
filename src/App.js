92f4972a8727289ba37a378673202d347cdd7a6e
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2023-01-11 16:52:08 +0200

    frontend: added components to the app component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import { Header } from './components/Header/Header'
+import { ToDoList } from './components/ToDoList/ToDoList'
+import './App.css'
+
 function App() {
+  const todoList = [
+    {
+      id: 1,
+      title: 'Learn React',
+      image:
+        'https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png',
+    },
+    {
+      id: 2,
+      title: 'Learn Redux',
+      image: 'https://cdn.iconscout.com/icon/free/png-512/redux-283024.png',
+    },
+    {
+      id: 3,
+      title: 'Learn Node',
+      image: 'https://cdn.iconscout.com/icon/free/png-512/node-js-1174925.png',
+    },
+    {
+      id: 4,
+      title: 'Learn Express',
+      image: 'https://cdn.iconscout.com/icon/free/png-512/express-283284.png',
+    },
+  ]
   return (
-    <div></div>
-  );
+    <div className='app'>
+      <Header />
+      <ToDoList todoList={todoList} />
+    </div>
+  )
 }
 
-export default App;
+export default App
