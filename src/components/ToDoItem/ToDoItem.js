c27bd175b2c27c7e0e02265f5cbc28a96c5d5a72
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2023-01-12 10:03:19 +0200

    frontend: added delete task functionality

--- src/components/ToDoItem/ToDoItem.js
+++ src/components/ToDoItem/ToDoItem.js
@@ -2,26 +2,36 @@ import { faCheck, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './todoItem.css'
 
-export const TodoItem = ({ todo, markTaskComplete }) => {
-    const handleClick = (e) => {
-        e.preventDefault()
-        console.log(e.currentTarget.id)
-        markTaskComplete(e.currentTarget.id)
-    }
-    return (        
-        <div className='todo-item' onClick={handleClick} id={todo.id}>
-            <img className='todo-image' src={todo.image} alt={todo.title} />
-            <div className='todo-info'>
+export const TodoItem = ({ todo, markTaskComplete, deleteTask }) => {
+  const handleClick = (id) => {
+    // e.preventDefault()
+    // console.log(e.currentTarget.id)
+    // markTaskComplete(e.currentTarget.id)
+    markTaskComplete(id)
+  }
 
-            <p className={todo.complete ? 'todo-title purple' : 'todo-title'}>{todo.title}</p>
-            {todo.complete &&
+  const handleDelete = (id) => {
+    deleteTask(id)
+  }
+
+  return (
+    <div className='todo-item'>
+      <div className='todo-item_details' onClick={() => handleClick(todo.id)}>
+        <img className='todo-image' src={todo.image} alt={todo.title} />
+        <div className='todo-info'>
+          <p className={todo.complete ? 'todo-title purple' : 'todo-title'}>
+            {todo.title}
+          </p>
+          {todo.complete && (
             <p className={todo.complete && 'complete'}>
-                <FontAwesomeIcon icon={faCheck} color="green"/> 
-            </p> }
-            </div>
-            <button className='btn btn-delete'>
-                <FontAwesomeIcon icon={faTrash} color="red"/>
-            </button>
+              <FontAwesomeIcon icon={faCheck} color='green' />
+            </p>
+          )}
         </div>
-    )
+      </div>
+      <button className='btn btn-delete' onClick={() => handleDelete(todo.id)}>
+        <FontAwesomeIcon icon={faTrash} color='red' />
+      </button>
+    </div>
+  )
 }
