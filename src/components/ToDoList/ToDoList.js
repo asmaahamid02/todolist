65e77175a9843129035ce5ca02f5b50111798a8f
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2023-01-12 09:17:31 +0200

    frontend: passed markTaskComplete as prop

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -1,14 +1,14 @@
 import { TodoItem } from '../ToDoItem/ToDoItem'
 import './todoList.css'
 
-export const ToDoList = ({ todoList }) => {
+export const ToDoList = ({ todoList , markTaskComplete}) => {
   return (
     <div className='container'>
       <h1 className='container-header'>Your ToDo List</h1>
       <div className='content-body'>      
           {todoList.length > 0 ? (
             todoList.map((todo) => (
-             <TodoItem todo={todo} key={todo.id} />
+             <TodoItem todo={todo} key={todo.id} markTaskComplete={markTaskComplete}/>
             ))
           ) : (
             <p className='no-todo'>No tasks in your list</p>
