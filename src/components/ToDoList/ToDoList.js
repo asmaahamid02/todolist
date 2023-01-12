f462f18a86327bce5d5eb57dcf1ed0a8ead5a134
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2023-01-12 09:38:11 +0200

    frontend: added incomplete tasks counter

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -1,18 +1,27 @@
 import { TodoItem } from '../ToDoItem/ToDoItem'
 import './todoList.css'
 
-export const ToDoList = ({ todoList , markTaskComplete}) => {
+export const ToDoList = ({ todoList, markTaskComplete }) => {
+    const incompleteTasks  = todoList.filter((item) => !item.complete).length 
+    const text = incompleteTasks > 0 ? `${incompleteTasks} incomplete tasks` : 'All tasks completed'
   return (
     <div className='container'>
-      <h1 className='container-header'>Your ToDo List</h1>
-      <div className='content-body'>      
-          {todoList.length > 0 ? (
-            todoList.map((todo) => (
-             <TodoItem todo={todo} key={todo.id} markTaskComplete={markTaskComplete}/>
-            ))
-          ) : (
-            <p className='no-todo'>No tasks in your list</p>
-          )}
+      <div className='container-header'>
+        <h1 className='container-header_title'>Your ToDo List</h1>       
+         <small className='container-header_subtitle'>{text}</small>                    
+      </div>
+      <div className='content-body'>
+        {todoList.length > 0 ? (
+          todoList.map((todo) => (
+            <TodoItem
+              todo={todo}
+              key={todo.id}
+              markTaskComplete={markTaskComplete}
+            />
+          ))
+        ) : (
+          <p className='no-todo'>No tasks in your list</p>
+        )}
       </div>
     </div>
   )
