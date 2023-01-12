43c75f0838bcf13126140dd68b329c65f9efd2ff
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2023-01-12 15:21:51 +0200

    frontend: added logout functionality

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,24 @@
 import './header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+import { useAuthContext } from '../../context/AuthContext';
+import {useNavigate} from 'react-router-dom'
 
 export const Header = () => {
+    const [authUser, setAuthUser] = useAuthContext()
+    const navigate = useNavigate()
+
+    const logout = () => {
+        localStorage.removeItem('user')
+        setAuthUser(false)
+        navigate('/login')
+    }
     return (
         <div className="header">
             <h1>ToDo List</h1>
+            <button className="btn btn-logout" onClick={logout}>
+                <FontAwesomeIcon icon={faArrowRightFromBracket} color="white"/>
+            </button>
         </div>
     );
 }
