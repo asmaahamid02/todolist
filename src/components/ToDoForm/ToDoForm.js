3f676251253929cb8f4f7f96a456fba47938673e
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2023-01-12 08:20:58 +0200

    frontend: added form validation

--- src/components/ToDoForm/ToDoForm.js
+++ src/components/ToDoForm/ToDoForm.js
@@ -1,20 +1,77 @@
 import './todoForm.css'
+import { useState, useEffect } from 'react'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 export const ToDoForm = () => {
-    return (
-        <div className='container'>
-            <h1 className='container-header'>Add New Task</h1>
-            <form className='form-body'>
-                <div className='form-group'>
-                    <label htmlFor='title'>Title</label>
-                    <input type='text' name='title' id='title' />
-                </div>
-                <div className='form-group'>
-                    <label htmlFor='image'>Image</label>
-                    <input type='file' name='image' id='image' />
-                </div>
-                <button className='btn submit-button'>Add</button>
-            </form>
+  const formik = useFormik({
+    initialValues: {
+      title: '',
+      image: null,
+    },
+    onSubmit: (values) => {
+      console.log(values)
+    },
+    validationSchema: Yup.object({
+      title: Yup.string().required('Title is required'),
+      image: Yup.mixed().required('Image is required'),
+    }),
+  })
+
+  return (
+    <div className='container'>
+      <h1 className='container-header'>Add New Task</h1>
+      <form
+        className='form-body'
+        onSubmit={(e) => {
+          formik.handleSubmit(e)
+        }}
+      >
+        <div
+          className={
+            formik.errors.title && formik.touched.title
+              ? 'form-group invalid'
+              : 'form-group'
+          }
+        >
+          <label htmlFor='title'>Title</label>
+          <input
+            type='text'
+            id='title'
+            placeholder='Title'
+            {...formik.getFieldProps('title')}
+          />
+          {formik.errors.title && formik.touched.title && (
+            <p className='error'>{formik.errors.title}</p>
+          )}
         </div>
-    )
+        <div
+          className={
+            formik.errors.image && formik.touched.image
+              ? 'form-group invalid'
+              : 'form-group'
+          }
+        >
+          <label htmlFor='image'>Image</label>
+          <input
+            type='file'
+            name='image'
+            id='image'
+            onChange={(e) => {
+              // console.log(e.target.files[0])
+              // setImageInput(URL.createObjectURL(e.target.files[0]))
+              formik.setFieldValue(
+                'image',
+                URL.createObjectURL(e.target.files[0])
+              )
+            }}
+          />
+          {formik.errors.image && formik.touched.image && (
+            <p className='error'>{formik.errors.image}</p>
+          )}
+        </div>
+        <button type='submit' className='btn submit-button'>Add</button>
+      </form>
+    </div>
+  )
 }
